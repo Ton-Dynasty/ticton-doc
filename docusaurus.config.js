65f12f2f5678dd0c6eeaffcf4c6fe584df2efe90
ipromise2324 <168allen55@gmail.com>
2024-01-01 18:51:25 +0800

    chore(docusaurus.config.js): change baseUrl variable for production environment to "/ticton-doc/"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.NODE_ENV === "production" ? "/Doc-Test/" : "/",
+  baseUrl: process.env.NODE_ENV === "production" ? "/ticton-doc/" : "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

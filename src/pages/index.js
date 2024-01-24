2e6ac5662b2f53b4192841d5931fb558cf90c0b3
ipromise2324 <168allen55@gmail.com>
2024-01-25 00:45:04 +0800

    chore(index.js): update Github link in HomepageHeader component

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="https://github.com/Ton-Dynasty/ticton-v0">
+            to="https://github.com/Ton-Dynasty">
             Github
           </Link>
         </div>
